62de39c74e4ad8077d99d575bc082ace15b9453c
alexYurin <sans.y@mail.ru>
2023-01-19 17:35:49 +0700

    edited styled config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier-scss'],
+  extends: [
+    'stylelint-config-sass-guidelines',
+    'stylelint-config-prettier-scss',
+  ],
+  rules: {
+    'selector-class-pattern': null,
+    'order/properties-alphabetical-order': null,
+    'scss/selector-no-redundant-nesting-selector': null,
+    'color-hex-length': 'long',
+    'max-nesting-depth': 4,
+  },
 }
