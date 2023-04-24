219409c15421b81e9e3a4e70781892e8f296f976
alexYurin <sans.y@mail.ru>
2023-04-24 19:44:03 +0700

    fixed

--- src/layouts/Chat/components/index.ts
+++ src/layouts/Chat/components/index.ts
@@ -1,3 +1,5 @@
+export { default as ChatMessage } from './Message'
+export { default as MessageList } from './MessageList'
 export { default as ChatContact } from './Contact'
 export { default as ChatContactList } from './ContactList'
 export { default as ChatCreateForm } from './CreateForm'
