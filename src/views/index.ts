91fe14adcfe97d03028e353734518b553b93b4a1
AlexanderYurin <sans.y@mail.ru>
2023-06-10 16:42:11 +0700

    fixed

--- src/views/index.ts
+++ src/views/index.ts
@@ -3,5 +3,3 @@ export { default as ErrorView } from './Error'
 export { default as NotFoundView } from './NotFound'
 export { default as SignInView } from './SignIn'
 export { default as SignUpView } from './SignUp'
-
-console.log('sdfsd')
